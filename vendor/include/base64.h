d7a5b61c7200bfe7926a4ad39d2e219a53f5b645
Andrew Anisimov <anisimov.new@gmail.com>
2020-02-23 15:28:41 +0300

    Add missing includes

--- vendor/include/base64.h
+++ vendor/include/base64.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stddef.h>
+
 int base64encode(const void *data_buf, size_t dataLength, char *result, size_t resultSize);
 
 int base64decode(const char *in, size_t inLen, unsigned char *out, size_t *outLen);
