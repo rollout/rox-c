fb82126a942bf01c79c04d42a7db8dab664ee2ed
Andrew Anisimov <anisimov.new@gmail.com>
2020-02-23 15:48:04 +0300

    With issue with __VA_ARGS__ when no args defined.

--- src/core/logging.h
+++ src/core/logging.h
@@ -9,8 +9,8 @@ void ROX_INTERNAL rox_log_warning(const char *file_name, int line, const char *f
 
 void ROX_INTERNAL rox_log_error(const char *file_name, int line, const char *fmt, ...);
 
-#define ROX_DEBUG(fmt, ...) rox_log_debug(__FILE__, __LINE__, fmt, __VA_ARGS__)
+#define ROX_DEBUG(fmt, ...) rox_log_debug(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
 
-#define ROX_WARN(fmt, ...) rox_log_warning(__FILE__, __LINE__, fmt, __VA_ARGS__)
+#define ROX_WARN(fmt, ...) rox_log_warning(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
 
-#define ROX_ERROR(fmt, ...) rox_log_error(__FILE__, __LINE__, fmt, __VA_ARGS__)
+#define ROX_ERROR(fmt, ...) rox_log_error(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
