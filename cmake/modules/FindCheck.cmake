85cd78418571535773144838211a19132430790f
Andrew Anisimov <anisimov.new@gmail.com>
2020-02-24 18:13:20 +0300

    Get FindCheck from https://github.com/libcheck/check/blob/master/doc/example/cmake/FindCheck.cmake
    Modify it a little, add Check::Check target definition.

--- cmake/modules/FindCheck.cmake
+++ cmake/modules/FindCheck.cmake
@@ -1,37 +1,61 @@
-﻿# - Find Check
-# Find the native Check headers and libraries.
+﻿# - Try to find the CHECK libraries
+#  Once done this will define
 #
-# CHECK_INCLUDE_DIRS    - where to find check.h, etc.
-# CHECK_LIBRARIES   - List of libraries when using Check.
-# CHECK_FOUND   - True if Check found.
-# Look for the header file.
-FIND_PATH(CHECK_INCLUDE_DIR check.h)
+#  CHECK_FOUND - system has check
+#  CHECK_INCLUDE_DIR - the check include directory
+#  CHECK_LIBRARIES - check library
+#
+#  This configuration file for finding libcheck is originally from
+#  the opensync project. The originally was downloaded from here:
+#  opensync.org/browser/branches/3rd-party-cmake-modules/modules/FindCheck.cmake
+#
+#  Copyright (c) 2007 Daniel Gollub <dgollub@suse.de>
+#  Copyright (c) 2007 Bjoern Ricks  <b.ricks@fh-osnabrueck.de>
+#
+#  Redistribution and use is allowed according to the terms of the New
+#  BSD license.
+#  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-if (CHECK_STATIC)
-    set(CHECK_LIBRARY_NAME ${CMAKE_STATIC_LIBRARY_PREFIX}check${CMAKE_STATIC_LIBRARY_SUFFIX})
-else ()
-    set(CHECK_LIBRARY_NAME ${CMAKE_SHARED_LIBRARY_PREFIX}checkDynamic${CMAKE_SHARED_LIBRARY_SUFFIX})
-endif ()
 
-# Look for the library.
-FIND_LIBRARY(CHECK_LIBRARY NAMES ${CHECK_LIBRARY_NAME})
+INCLUDE(FindPkgConfig)
 
-# Handle the QUIETLY and REQUIRED arguments and set CHECK_FOUND to TRUE if all listed variables are TRUE.
-INCLUDE(FindPackageHandleStandardArgs)
+# Take care about check.pc settings
+PKG_SEARCH_MODULE(CHECK Check)
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(CHECK REQUIRED_VARS CHECK_LIBRARY CHECK_INCLUDE_DIR)
+# Look for CHECK include dir and libraries
+IF (NOT CHECK_FOUND)
+    IF (CHECK_INSTALL_DIR)
+        MESSAGE(STATUS "Using override CHECK_INSTALL_DIR to find Check")
+        SET(CHECK_INCLUDE_DIR "${CHECK_INSTALL_DIR}/include")
+        SET(CHECK_INCLUDE_DIRS "${CHECK_INCLUDE_DIR}")
+        FIND_LIBRARY(CHECK_LIBRARY NAMES check PATHS "${CHECK_INSTALL_DIR}/lib")
+        FIND_LIBRARY(COMPAT_LIBRARY NAMES compat PATHS "${CHECK_INSTALL_DIR}/lib")
+        SET(CHECK_LIBRARIES "${CHECK_LIBRARY}" "${COMPAT_LIBRARY}")
+    ELSE (CHECK_INSTALL_DIR)
+        FIND_PATH(CHECK_INCLUDE_DIR check.h)
+        FIND_LIBRARY(CHECK_LIBRARIES NAMES check)
+    ENDIF (CHECK_INSTALL_DIR)
 
-# Copy the results to the output variables.
-IF (CHECK_FOUND)
-    SET(CHECK_LIBRARIES ${CHECK_LIBRARY})
-    SET(CHECK_INCLUDE_DIRS ${CHECK_INCLUDE_DIR})
-    if (NOT TARGET Check::Check)
-        add_library(Check::Check UNKNOWN IMPORTED)
-        set_target_properties(Check::Check PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CHECK_INCLUDE_DIR}")
-        set_property(TARGET Check::Check APPEND PROPERTY IMPORTED_LOCATION "${CHECK_LIBRARY}")
-    endif ()
-ELSE (CHECK_FOUND)
-    SET(CHECK_LIBRARIES)
-    SET(CHECK_INCLUDE_DIRS)
-ENDIF (CHECK_FOUND)
-MARK_AS_ADVANCED(CHECK_INCLUDE_DIRS CHECK_LIBRARIES)
+    IF (CHECK_INCLUDE_DIR AND CHECK_LIBRARIES)
+        SET(CHECK_FOUND 1)
+        if (NOT TARGET Check::Check)
+            add_library(Check::Check UNKNOWN IMPORTED)
+            set_target_properties(Check::Check PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CHECK_INCLUDE_DIR}")
+            set_property(TARGET Check::Check APPEND PROPERTY IMPORTED_LOCATION "${CHECK_LIBRARIES}")
+        endif ()
+        IF (NOT Check_FIND_QUIETLY)
+            MESSAGE(STATUS "Found CHECK: ${CHECK_LIBRARIES}")
+        ENDIF (NOT Check_FIND_QUIETLY)
+    ELSE (CHECK_INCLUDE_DIR AND CHECK_LIBRARIES)
+        IF (Check_FIND_REQUIRED)
+            MESSAGE(FATAL_ERROR "Could NOT find CHECK")
+        ELSE (Check_FIND_REQUIRED)
+            IF (NOT Check_FIND_QUIETLY)
+                MESSAGE(STATUS "Could NOT find CHECK")
+            ENDIF (NOT Check_FIND_QUIETLY)
+        ENDIF (Check_FIND_REQUIRED)
+    ENDIF (CHECK_INCLUDE_DIR AND CHECK_LIBRARIES)
+ENDIF (NOT CHECK_FOUND)
+
+# Hide advanced variables from CMake GUIs
+MARK_AS_ADVANCED(CHECK_INCLUDE_DIR CHECK_LIBRARIES)
