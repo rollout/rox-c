00b5fc6166f8e041c77d78beb6c2a17ef6eed7e5
Andrew Anisimov <anisimov.new@gmail.com>
2020-02-26 20:06:13 +0300

    get_device_id impl for FreeBSD

--- src/device.c
+++ src/device.c
@@ -1,4 +1,6 @@
+#include <stdbool.h>
 #include "os.h"
+#include "device.h"
 #include "util.h"
 #include "core/logging.h"
 
@@ -15,23 +17,22 @@
 #include <IOKit/IOKitLib.h>
 #endif
 
-#include <stdbool.h>
-
-#include "device.h"
-#include "util.h"
-
-#define UUID_STRING_LENGTH 36
 #define ROX_MACHINE_ID_BUFFER_LENGTH 128
 
 static const char *ROX_DEVICE_ID = NULL;
 
 static char *get_device_id() {
 #ifdef ROX_FREE_BSD
-    // TODO: implement!
+    char buffer[ROX_MACHINE_ID_BUFFER_LENGTH];
+    if (rox_file_read_b("/etc/hostid", (unsigned char*)buffer, ROX_MACHINE_ID_BUFFER_LENGTH) == -1) {
+        ROX_ERROR("Failed to read /etc/hostid");
+        return NULL;
+    }
+    return mem_copy_str(buffer);
 #elif defined(ROX_LINUX)
     char buffer[ROX_MACHINE_ID_BUFFER_LENGTH];
     if (rox_file_read_b("/var/lib/dbus/machine-id", (unsigned char*)buffer, ROX_MACHINE_ID_BUFFER_LENGTH) == -1) {
-        ROX_ERROR("Failed to read file /var/lib/dbus/machine-id");
+        ROX_ERROR("Failed to read /var/lib/dbus/machine-id");
         return NULL;
     }
     return mem_copy_str(buffer);
@@ -41,7 +42,7 @@ static char *get_device_id() {
     HKEY key = NULL;
     if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Cryptography", 0,
                      KEY_READ | KEY_WOW64_64KEY, &key) == ERROR_SUCCESS) {
-        char buffer[UUID_STRING_LENGTH + 1];
+        char buffer[ROX_MACHINE_ID_BUFFER_LENGTH + 1];
         DWORD size = sizeof(buffer);
         bool found = (RegQueryValueEx(key, "MachineGuid", NULL, NULL, (LPBYTE) buffer, &size) == ERROR_SUCCESS);
         RegCloseKey(key);
