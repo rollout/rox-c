827ae082554369ec10fa3411aa08ed81c40336ed
Andrew Anisimov <anisimov.new@gmail.com>
2020-02-27 21:15:38 +0300

    Demo build script.

---
+++ demo/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ ! -d "${ROLLOUT_SDK_ROOT}" ]; then
+  ROLLOUT_SDK_ROOT=/usr/local/rollout-sdk
+fi
+
+echo "Looking for Rollout SDK in ${ROLLOUT_SDK_ROOT} (specify ROLLOUT_SDK_ROOT variable to override it)"
+
+if [ ! -d "${ROLLOUT_SDK_ROOT}" ]; then
+  echo "Directory ${ROLLOUT_SDK_ROOT} not exists. Existing."
+  exit 1
+fi
+
+echo "Building Rollout SDK demo..."
+rm -rf build
+mkdir -p build
+cd build || exit
+cmake .. -DCMAKE_BUILD_TYPE=Release -DROLLOUT_SDK_ROOT="${ROLLOUT_SDK_ROOT}"
+make
+
+echo "Rollout SDK demo build finished."
