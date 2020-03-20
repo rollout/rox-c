4abda8f9980c646393507ee6f895c365af671ce1
Andrew Anisimov <anisimov.new@gmail.com>
2020-03-20 08:13:37 +0300

    C++ logging demo.

---
+++ demo/logging.cpp
@@ -0,0 +1,64 @@
+#include <rollout.hpp>
+#include <cstdio>
+#include <cassert>
+#include <iostream>
+#include <fstream>
+
+#define DEFAULT_API_KEY "5e6a3533d3319d76d1ca33fd"
+#define DEFAULT_DEV_MODE_KEY "297c23e7fcb68e54c513dcca"
+#define LOG_FILE_PATH "./logging-output-cpp.log"
+
+static void _file_logging_handler(void *target, RoxLogMessage *message) {
+    assert(target);
+    assert(message);
+    auto *stream = (std::ofstream *) target;
+    *stream << "(" << message->level_name << ")" << " " << message->message << "\n";
+    stream->flush();
+}
+
+class StreamLoggingHandler : public Rox::LogMessageHandlerInterface {
+    std::ofstream *_stream;
+public:
+    explicit StreamLoggingHandler(std::ofstream *stream) : _stream(stream) {}
+
+    void HandleLogMessage(Rox::LogMessage *message) override {
+        *_stream << "(" << message->level_name << ")" << " " << message->message << "\n";
+        _stream->flush();
+    }
+};
+
+int main(int argc, char **argv) {
+
+    std::ofstream stream(LOG_FILE_PATH);
+    if (!stream.is_open()) {
+        std::cerr << "Cannot open " << LOG_FILE_PATH << "\n";
+        return -1;
+    }
+
+    StreamLoggingHandler loggingHandler(&stream);
+
+    Rox::Logging::SetLogLevel(RoxLogLevelDebug);
+    Rox::Logging::SetLogMessageHandler(&loggingHandler);
+
+    Rox::Flag *demoFlag = Rox::Flag::Create("demo.demoFlag");
+    Rox::Options *options = Rox::OptionsBuilder()
+            .SetDevModeKey(DEFAULT_DEV_MODE_KEY)
+            .Build();
+
+    Rox::Setup(DEFAULT_API_KEY, options);
+
+    char c = 'Y';
+    while (c != 'n' && c != 'N') {
+        printf("Demo flag is %s\n", demoFlag->IsEnabled() ? "ON" : "OFF");
+        printf("Continue? (Y/n):");
+        c = fgetc(stdin);
+        if (c == 0x0A) { // Enter key pressed
+            c = 'Y';
+        }
+        getchar(); // read dummy character to clear input buffer, which inserts after character input
+        printf("\n");
+    }
+
+    Rox::Shutdown();
+    return 0;
+}
